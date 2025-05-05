ddd8831b81423dbda6bf97c38941da72a917cdec
Eric Ekholm <eric.ekholm@gmail.com>
2025-05-05 15:40:00 -0400

    add logging middleware

--- server/routes.go
+++ server/routes.go
@@ -9,14 +9,13 @@ import (
 	daren "github.com/wintergathering/daren2"
 )
 
+// html routes ---------------
 func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
 	//set header to html
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	headerStatus := http.StatusOK
 	w.WriteHeader(headerStatus)
-
-	s.logRequest(r, headerStatus, nil)
 
 	s.Templates.ExecuteTemplate(w, "index.html", nil)
 }
@@ -28,18 +27,14 @@ func (s *Server) handleSuccess(w http.ResponseWriter, r *http.Request) {
 	headerStatus := http.StatusOK
 	w.WriteHeader(headerStatus)
 
-	s.logRequest(r, headerStatus, nil)
-
 	s.Templates.ExecuteTemplate(w, "dare_success.html", nil)
 }
 
-// html routes ---------------
 func (s *Server) handleCreateDare(w http.ResponseWriter, r *http.Request) {
 	d := &daren.Dare{}
 
 	//check request method
 	if r.Method != http.MethodPost {
-		s.logRequest(r, http.StatusMethodNotAllowed, nil)
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -49,7 +44,6 @@ func (s *Server) handleCreateDare(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 
 	if err != nil {
-		s.logRequest(r, http.StatusBadRequest, err)
 		w.WriteHeader(http.StatusBadRequest)
 		s.Templates.ExecuteTemplate(w, "error.html", nil)
 		return
@@ -63,15 +57,12 @@ func (s *Server) handleCreateDare(w http.ResponseWriter, r *http.Request) {
 
 	//keeping this for now, but will want to return a real page in the future
 	if err != nil {
-		s.logRequest(r, http.StatusInternalServerError, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	//redirect to /success
-	s.logRequest(r, http.StatusSeeOther, nil)
-	w.WriteHeader(http.StatusSeeOther)
 	http.Redirect(w, r, "/success", http.StatusSeeOther)
 
 }
@@ -80,23 +71,19 @@ func (s *Server) handleGetCreateDare(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	s.logRequest(r, http.StatusOK, nil)
-
 	s.Templates.ExecuteTemplate(w, "create_dare.html", nil)
 }
 
-func (s *Server) HandleGetRandomDare(w http.ResponseWriter, r *http.Request) {
+func (s *Server) handleGetRandomDare(w http.ResponseWriter, r *http.Request) {
 	dare, err := s.DareService.GetRandomDare()
 
 	if errors.Is(err, daren.ErrNoDare) {
-		s.logRequest(r, http.StatusNotFound, err)
 		w.WriteHeader(http.StatusNotFound)
 		s.Templates.ExecuteTemplate(w, "no_dares.html", nil)
 		return
 	}
 
 	if err != nil {
-		s.logRequest(r, http.StatusInternalServerError, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		s.Templates.ExecuteTemplate(w, "error.html", nil)
 		return
@@ -105,13 +92,11 @@ func (s *Server) HandleGetRandomDare(w http.ResponseWriter, r *http.Request) {
 	err = s.DareService.MarkDareSeen(dare.ID)
 
 	if err != nil {
-		s.logRequest(r, http.StatusInternalServerError, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		s.Templates.ExecuteTemplate(w, "error.html", nil)
 		return
 	}
 
-	s.logRequest(r, http.StatusOK, nil)
 	w.WriteHeader(http.StatusOK)
 	s.Templates.ExecuteTemplate(w, "single_dare.html", dare)
 
@@ -121,20 +106,17 @@ func (s *Server) handleGetAllDares(w http.ResponseWriter, r *http.Request) {
 	dares, err := s.DareService.GetAllDares()
 
 	if err != nil {
-		s.logRequest(r, http.StatusInternalServerError, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		s.Templates.ExecuteTemplate(w, "error.html", nil)
 		return
 	}
 
 	if len(dares) == 0 {
-		s.logRequest(r, http.StatusNotFound, err)
 		w.WriteHeader(http.StatusNotFound)
 		s.Templates.ExecuteTemplate(w, "no_dares.html", nil)
 		return
 	}
 
-	s.logRequest(r, http.StatusOK, nil)
 	w.WriteHeader(http.StatusOK)
 	s.Templates.ExecuteTemplate(w, "all_dares.html", dares)
 }
--- server/server.go
+++ server/server.go
@@ -68,19 +68,19 @@ func (s *Server) registerRoutes() {
 	//TODO -- use custom css here
 
 	//html
-	s.Router.HandleFunc("GET /", s.handleIndex)
-	s.Router.HandleFunc("GET /success", s.handleSuccess)
-	s.Router.HandleFunc("GET /create", s.handleGetCreateDare)
-	s.Router.HandleFunc("POST /create", s.handleCreateDare)
-	s.Router.HandleFunc("GET /all", s.handleGetAllDares)
-	s.Router.HandleFunc("GET /random", s.HandleGetRandomDare)
+	s.Router.Handle("GET /", s.LoggingMiddleware(http.HandlerFunc(s.handleIndex)))
+	s.Router.Handle("GET /success", s.LoggingMiddleware(http.HandlerFunc(s.handleSuccess)))
+	s.Router.Handle("GET /create", s.LoggingMiddleware(http.HandlerFunc(s.handleGetCreateDare)))
+	s.Router.Handle("POST /create", s.LoggingMiddleware(http.HandlerFunc(s.handleCreateDare)))
+	s.Router.Handle("GET /all", s.LoggingMiddleware(http.HandlerFunc(s.handleGetAllDares)))
+	s.Router.Handle("GET /random", s.LoggingMiddleware(http.HandlerFunc(s.handleGetRandomDare)))
 
-	//api
-	s.Router.HandleFunc("POST /api/v1/dare/create", s.handleApiCreateDare)
-	s.Router.HandleFunc("GET /api/v1/dare/id/{id}", s.handleApiGetDareById)
-	s.Router.HandleFunc("GET /api/v1/dare/random", s.handleApiGetRandomDare)
-	s.Router.HandleFunc("GET /api/v1/dare/all", s.handleApiGetAllDares)
-	s.Router.HandleFunc("DELETE /api/v1/dare/id/{id}", s.handleApiDeleteDare)
+	//api with logging
+	s.Router.Handle("POST /api/v1/dare/create", s.LoggingMiddleware(http.HandlerFunc(s.handleApiCreateDare)))
+	s.Router.Handle("GET /api/v1/dare/id/{id}", s.LoggingMiddleware(http.HandlerFunc(s.handleApiGetDareById)))
+	s.Router.Handle("GET /api/v1/dare/random", s.LoggingMiddleware(http.HandlerFunc(s.handleApiGetRandomDare)))
+	s.Router.Handle("GET /api/v1/dare/all", s.LoggingMiddleware(http.HandlerFunc(s.handleApiGetAllDares)))
+	s.Router.Handle("DELETE /api/v1/dare/id/{id}", s.LoggingMiddleware(http.HandlerFunc(s.handleApiDeleteDare)))
 }
 
 func (s *Server) Run() {
@@ -96,27 +96,23 @@ func (s *Server) Run() {
 	s.Srvr.ListenAndServe()
 }
 
-// log utility
-func (s *Server) logRequest(r *http.Request, statusCode int, err error) {
-	var e string
-	if err == nil {
-		e = ""
-	} else {
-		e = err.Error()
-	}
-	g := slog.Group(
-		"request_details",
-		slog.Time("timestamp", time.Now()),
-		slog.String("method", r.Method),
-		slog.String("path", r.URL.Path),
-		slog.String("remote_addr", r.RemoteAddr),
-		slog.Int("status_code", statusCode),
-		slog.Any("error", e),
-	)
+// logging middleware
+func (s *Server) LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 
-	s.Logger.Info("request received",
-		g,
-	)
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+
+		s.Logger.Info("request received",
+			slog.Time("timestamp", time.Now()),
+			slog.String("method", r.Method),
+			slog.String("path", r.URL.Path),
+			slog.String("remote_addr", r.RemoteAddr),
+			slog.Duration("duration", duration),
+		)
+	})
 }
 
 // utility ------------
